36527136fe99b509f150e98f1068755c1612976a
Anthonyj45 <Amorontaespinal9338@conestogac.on.ca>
2023-12-17 15:47:26 -0500

    I have created the board game where the game will be executed

--- Snake Game/snake.cpp
+++ Snake Game/snake.cpp
@@ -1,8 +1,37 @@
 #include <stdio.h>
 
+//Constant Declarations
+const int kRows = 20;
+const int kColumns = 20;
+
+//Functions Prototypes
+void printBoard(void);
+
 int main(void)
 {
-
-
+	printBoard();
 	return 0;
+}
+
+void printBoard(void)
+{
+	for (int counter = 0; counter < kRows; counter++)
+	{
+		for (int sCounter = 0; sCounter < kColumns; sCounter++)
+		{
+			if (counter == 0 || counter == kRows - 1)
+			{
+				printf("-");
+			}
+			else if (sCounter == 0 || sCounter == kColumns - 1)
+			{
+				printf("|");
+			}
+			else
+			{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
 }
