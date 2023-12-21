1e37f9c6f91c8c22237c3baf716dac65c15b4209
Anthonyj45 <Amorontaespinal9338@conestogac.on.ca>
2023-12-20 22:17:55 -0500

    I have improved the code, now is working better and the snake is adding a tail each time the fruit is eaten. However, I have to keep the track of the last position of my tail to make it more realistic because for the moment the tail keeps a form in one direction

--- Snake Game/snake.cpp
+++ Snake Game/snake.cpp
@@ -21,58 +21,57 @@ struct mySnake
 	int headY;
 	int length;
 	int direction;
-	int bodyX[body_length];
-	int bodyY[body_length];
+};
+
+struct snakeBody
+{
+	int tailX[body_length];
+	int tailY[body_length];
 };
 
 struct myFruit
 {
 	int positionX;
 	int positionY;
+	int eaten;
 };
 
 //Prototypes Declarations
-void print_board(struct mySnake snake, int fruitX, int fruitY);
-void generate_fruit(struct mySnake* snake, int* fruitX, int* fruitY);
+void generate_fruit(struct mySnake* snake, struct myFruit* fruit, struct snakeBody* body);
+void print_board(struct mySnake snake, struct snakeBody body, struct myFruit fruit);
 int get_input(void);
 void move_snake(struct mySnake* snake);
-int check_collision(struct mySnake snake);
-void increase_snake_length(struct mySnake* snake);
-
-/*ALERT:
-* The code is not displaying the tail properly. So it is necessary make
-* some modifications to make the tail follow the head of the Snake.
-*/
+int check_collision(struct mySnake snake, struct snakeBody body);
 
 int main(void)
 {
-	struct mySnake snake = { width / 2, height / 2, 1, 0, {0}, {0} };
-	struct myFruit fruit = { 0, 0 };
+	struct mySnake snake = { width / 2, height / 2, 1, 0 };
+	struct snakeBody body = { {0}, {0} };
+	struct myFruit fruit = { 0, 0, 0 };
 
 	int game_over = 0;
 
-	generate_fruit(&snake, &fruit.positionX, &fruit.positionY);
+	generate_fruit(&snake, &fruit, &body);
 
 	while (!game_over)
 	{
-
-		if (snake.headX == fruit.positionX && snake.headY == fruit.positionY)
-		{
-			increase_snake_length(&snake);
-			generate_fruit(&snake, &fruit.positionX, &fruit.positionY);
-		}
-
-		move_snake(&snake);
-
-		print_board(snake, fruit.positionX, fruit.positionY);
-
 		int direction = get_input();
 		if (direction)
 		{
 			snake.direction = direction;
 		}
+		move_snake(&snake);
 
-		game_over = check_collision(snake);
+		if (snake.headX == fruit.positionX && snake.headY == fruit.positionY)
+		{
+			fruit.eaten = 1;
+			snake.length++;
+			generate_fruit(&snake, &fruit, &body);
+		}
+
+		print_board(snake, body, fruit);
+
+		game_over = check_collision(snake, body);
 
 		Sleep(100);
 	}
@@ -80,13 +79,7 @@ int main(void)
 	return 0;
 }
 
-/*
-* Print_board function will take three parameters and will create a Board
-* Where the player will play the Snake Game. Also it will create the
-* Snake's body
-*/
-
-void print_board(struct mySnake snake, int fruitX, int fruitY)
+void print_board(struct mySnake snake, struct snakeBody body, struct myFruit fruit)
 {
 	system("cls");
 
@@ -95,44 +88,39 @@ void print_board(struct mySnake snake, int fruitX, int fruitY)
 		for (int sCounter = 0; sCounter < width; sCounter++)
 		{
 			int isBodyPart = 0;
-
 			if (counter == snake.headY && sCounter == snake.headX)
 			{
 				printf("O");
-			}
-			else
-			{
-				for (int tCounter = 0; tCounter < snake.length; tCounter++)
+				isBodyPart = 1;
+				int counterTailX = counter;
+				int counterTailY = sCounter;
+				if (fruit.eaten == 1)
 				{
-					if (snake.bodyX[tCounter] == sCounter && snake.bodyY[tCounter] == counter)
+					for (int tCounter = 0; tCounter < snake.length; tCounter++)
 					{
 						printf("o");
-						isBodyPart = 1;
 					}
-				}
-
-				if (!isBodyPart)
-				{
-					if (counter == 0 || counter == height - 1 || sCounter == 0 || sCounter == width - 1)
-					{
-						printf("#");
-					}
-
-					else if (counter == fruitY && sCounter == fruitX)
-					{
-						printf("*");
-					}
-					else
-					{
-						printf(" ");
-					}
+					isBodyPart = 1;
 				}
 			}
-
+			if (!isBodyPart)
+			{
+				if (counter == 0 || counter == height - 1 || sCounter == 0 || sCounter == width - 1)
+				{
+					printf("#");
+				}
+				else if (counter == fruit.positionY && sCounter == fruit.positionX)
+				{
+					printf("*");
+				}
+				else
+				{
+					printf(" ");
+				}
+			}
 		}
 		printf("\n");
 	}
-
 }
 
 /*
@@ -140,35 +128,29 @@ void print_board(struct mySnake snake, int fruitX, int fruitY)
 * the snake
 */
 
-
-void generate_fruit(struct mySnake* snake, int* fruitX, int* fruitY)
+void generate_fruit(struct mySnake* snake, struct myFruit* fruit, struct snakeBody* body)
 {
 	int collision = 0;
 
-	//Srand function seeds the random number generator that is used by the function
-	//rand
 	srand((unsigned int)time(NULL));
 
 	do
 	{
-		collision = 0;
+		fruit->positionX = rand() % (width - 2) + 1;
+		fruit->positionY = rand() % (height - 2) + 1;
 
-		*fruitX = rand() % (width - 2) + 1;
-		*fruitY = rand() % (height - 2) + 1;
-
-		if (*fruitX == snake->headX && *fruitY == snake->headY)
+		if (fruit->positionX == snake->headX && fruit->positionY == snake->headY)
 		{
 			collision = 1;
 		}
 
 		for (int counter = 0; counter < snake->length; counter++)
 		{
-			if (*fruitX == snake->bodyX[counter] && *fruitY == snake->bodyY[counter])
+			if (fruit->positionX == body->tailX[counter] && fruit->positionY == body->tailY[counter])
 			{
 				collision = 1;
 				break;
 			}
-
 		}
 
 	} while (collision);
@@ -183,11 +165,9 @@ void generate_fruit(struct mySnake* snake, int* fruitX, int* fruitY)
 int get_input(void)
 {
 	int key = 0;
-
 	if (_kbhit())
 	{
 		char move = _getch();
-
 		switch (move)
 		{
 		case 'w':
@@ -204,7 +184,6 @@ int get_input(void)
 			break;
 		}
 	}
-
 	return key;
 }
 
@@ -216,45 +195,24 @@ int get_input(void)
 
 void move_snake(struct mySnake* snake)
 {
-	int previousTailX = snake->bodyX[snake->length - 1];
-	int previousTailY = snake->bodyY[snake->length - 1];
-
 	switch (snake->direction)
 	{
 	case 1:
 		snake->headY--;
-		snake->bodyY[0]--;
 		break;
 	case 2:
 		snake->headY++;
-		snake->bodyY[0]++;
 		break;
 	case 3:
 		snake->headX--;
-		snake->bodyX[0]--;
 		break;
 	case 4:
 		snake->headX++;
-		snake->bodyX[0]++;
 		break;
 	}
-
-	for (int counter = snake->length - 1; counter > 0; counter--)
-	{
-		snake->bodyX[counter] = snake->bodyX[counter - 1];
-		snake->bodyY[counter] = snake->bodyY[counter - 1];
-	}
-	snake->bodyX[0] = previousTailX;
-	snake->bodyY[0] = previousTailY;
-
 }
 
-/*
-* It is necessary to use this function to check the colition with the Wall.
-* If the snake collides with the Wall, is Game Over
-*/
-
-int check_collision(struct mySnake snake)
+int check_collision(struct mySnake snake, struct snakeBody body)
 {
 	if (snake.headX == 0 || snake.headX == width - 1 || snake.headY == 0 || snake.headY == height - 1)
 	{
@@ -263,39 +221,10 @@ int check_collision(struct mySnake snake)
 
 	for (int counter = 0; counter < snake.length; counter++)
 	{
-		if (snake.headX == snake.bodyX[counter] && snake.headY == snake.bodyY[counter])
+		if (snake.headX == body.tailX[counter] && snake.headY == body.tailY[counter])
 		{
 			return 1;
 		}
 	}
-
 	return 0;
-}
-
-/*
-* While more you eat, bigger you will be
-*/
-
-void increase_snake_length(struct mySnake* snake)
-{
-	snake->length++;
-	switch (snake->direction)
-	{
-	case 1:
-		snake->bodyX[snake->length - 1] = snake->headX;
-		snake->bodyY[snake->length - 1] = snake->headY + 1;
-		break;
-	case 2:
-		snake->bodyX[snake->length - 1] = snake->headX;
-		snake->bodyY[snake->length - 1] = snake->headY - 1;
-		break;
-	case 3:
-		snake->bodyX[snake->length - 1] = snake->headX + 1;
-		snake->bodyY[snake->length - 1] = snake->headY;
-		break;
-	case 4:
-		snake->bodyX[snake->length - 1] = snake->headX - 1;
-		snake->bodyY[snake->length - 1] = snake->headY;
-		break;
-	}
 }
